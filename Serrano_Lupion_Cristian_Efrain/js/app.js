df1fda638e892b4612fd7b866910cf4cdf5fd34d
Cristian-Serrano <86664202+Cristian-Serrano@users.noreply.github.com>
2023-05-16 10:31:31 +0200

    fdsfgdsg
    fgdf

---
+++ Serrano_Lupion_Cristian_Efrain/js/app.js
@@ -0,0 +1,42 @@
+//si hay algun elemento vacío se muestra un alert
+function hacerClick() {
+    var email = document.getElementById("email").value;
+    var usuario = document.getElementById("usuario").value;
+    var password = document.getElementById("password").value;
+    var password2 = document.getElementById("password2").value;
+    var checkeado = document.getElementById("terminos").checked;
+
+    var usuarioDatos = {email:email, user:usuario, pass:password};
+    var usuarioDatosJSON = JSON.stringify(usuarioDatos);
+
+    if (email === "" || usuario === "" || password === "" || password2 === "") {
+        Swal.fire({
+            title: '¡Oops!',
+            text: '¡No puedes dejar ningún campo vacío!',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          });
+    }else if (password !== password2){ //si las dos contraseñas no coinciden
+        Swal.fire({
+            title: '¡Oops!',
+            text: '¡Las contraseñas no coinciden!',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          });
+    } else if (checkeado === false) {
+        Swal.fire({
+            title: '¡Oops!',
+            text: '¡Debes aceptar los terminos y condiciones!',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          });
+    } else
+    {
+        Swal.fire({
+            title: '¡Correcto!',
+            text: 'El resultado en Json es: '+usuarioDatosJSON,
+            icon: 'success',
+            confirmButtonText: 'Ok'
+          });
+    }
+}
